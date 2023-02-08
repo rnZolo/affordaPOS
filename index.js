771c6529b16d3c3ada3ded79aabaec1e3d1ccca0
Jeffrey Lonzanida <111095026+ifejinity@users.noreply.github.com>
2023-02-08 19:21:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// responsive nav bar
+$('.menu-toggle').click(function(){
+   $(".nav").toggleClass("mobile-nav");
+   $(this).toggleClass("is-active");
+});
+
+// Nav effect
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("mynav").style.backgroundColor = "#121013";
+    } else {
+        document.getElementById("mynav").style.backgroundColor = "transparent";
+    }
+}
