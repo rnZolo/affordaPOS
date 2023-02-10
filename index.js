8f9feaf81dc12efd4e3d4ce32a5c072ecb99d405
Jeffrey Lonzanida <111095026+ifejinity@users.noreply.github.com>
2023-02-10 15:15:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ function scrollFunction() {
         document.getElementById("mynav").style.backgroundColor = "transparent";
     }
 }
+
+// Modal Setup
+var modal = document.getElementById('modal');
+
+var modalClose = document.getElementById('modal-close');
+modalClose.addEventListener('click', function() { 
+  modal.style.display = "none";
+});
+
+// global handler
+document.addEventListener('click', function (e) { 
+  if (e.target.className.indexOf('modal-target') !== -1) {
+      var img = e.target;
+      var modalImg = document.getElementById("modal-content");
+      var captionText = document.getElementById("modal-caption");
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+   }
+});
