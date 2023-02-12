2e58894d3ce2c4f7b93fa97d54302f7040b6533c
Jeffrey Lonzanida <111095026+ifejinity@users.noreply.github.com>
2023-02-12 14:32:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ $('.menu-toggle').click(function(){
 // Nav effect
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("mynav").style.backgroundColor = "#F1F1F1";
-    } else {
-        document.getElementById("mynav").style.backgroundColor = "transparent";
-    }
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.querySelector(".navbar").style.backgroundColor = "#F1F1F1";
+        } else {
+            document.querySelector(".navbar").style.backgroundColor = "transparent";
+        }
 }
 
 // Modal Setup
@@ -24,12 +24,41 @@ modalClose.addEventListener('click', function() {
 
 // global handler
 document.addEventListener('click', function (e) { 
-  if (e.target.className.indexOf('modal-target') !== -1) {
-      var img = e.target;
-      var modalImg = document.getElementById("modal-content");
-      var captionText = document.getElementById("modal-caption");
-      modal.style.display = "block";
-      modalImg.src = img.src;
-      captionText.innerHTML = img.alt;
-   }
+    if (e.target.className.indexOf('modal-target') !== -1) {
+        var img = e.target;
+        var modalImg = document.getElementById("modal-content");
+        var captionText = document.getElementById("modal-caption");
+        modal.style.display = "block";
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+    }
 });
+
+// darkmode
+var app = document.getElementsByTagName("BODY")[0];
+    if (localStorage.lightMode == "dark") {
+        app.setAttribute("light-mode", "dark");
+}
+    
+function toggle_light_mode() {
+    var app = document.getElementsByTagName("BODY")[0];
+    if (localStorage.lightMode == "dark") {
+        localStorage.lightMode = "light";
+        app.setAttribute("light-mode", "light");
+    } else {
+        localStorage.lightMode = "dark";
+        app.setAttribute("light-mode", "dark");
+    }
+}
+
+window.addEventListener(
+    "storage",
+    function () {
+        if (localStorage.lightMode == "dark") {
+            app.setAttribute("light-mode", "dark");
+        } else {
+            app.setAttribute("light-mode", "light");
+        }
+    },
+    false
+);
